ada68b4c92a08321ab6a53aafdf2af24d79eb5a1
Chris Laughlin <cjlaughlin@gmail.com>
2017-05-15 10:39:45 -0700

    asdf

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="apple-touch-fullscreen" content="yes" />
 	</head>
-
+<!-- but wau -->
 	<!-- this makes the addressbar dissppear test edit-->
 	<!-- can you see this -->
 	<!-- another test -->
